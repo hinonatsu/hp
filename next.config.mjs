bcfb659f14ae00e30b86aac8184f57fe8e0e31e4
Natsuki Hino <ntk.shrike@gmail.com>
2026-07-07 13:03:48 +0900

    Deploy site with GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+const isGithubPagesBuild = process.env.GITHUB_ACTIONS === "true" && repositoryName !== "";
+const isUserOrOrgPage = repositoryName.endsWith(".github.io");
+const githubPagesBasePath = isGithubPagesBuild && !isUserOrOrgPage ? `/${repositoryName}` : "";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: githubPagesBasePath,
+  assetPrefix: githubPagesBasePath ? `${githubPagesBasePath}/` : undefined
+};
 
 export default nextConfig;
